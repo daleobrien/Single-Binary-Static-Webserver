5529baa5de7a7092a2534f824608cc7b152465bd
Dale O'Brien <daleobrien@users.noreply.github.com>
2026-07-26 18:56:39 +1000

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-docker build -f "rust-server/Dockerfile" -t app-rust .
-docker run -p 3000:3000 --rm app-rust
+if [ "${1:-}" = "--docker" ]; then
+    docker build -f "rust-server/Dockerfile" -t app-rust .
+    docker run -p 3000:3000 --rm app-rust
+else
+    cd rust-server && cargo test && cargo run --release
+fi
